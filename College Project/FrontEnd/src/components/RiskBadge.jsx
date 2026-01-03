6eed5ddd2a3c2e8622ef94b58854c421b3861a4b
rajpatel-7 <23010101298@darshan.ac.in>
2026-01-03 09:23:22 +0530

    commit

--- College Project/FrontEnd/src/components/RiskBadge.jsx
+++
@@ -1,8 +0,0 @@
-export default function RiskBadge({ category, probability }) {
-  return (
-    <div className={`badge ${category.toLowerCase().replace(" ", "-")}`}>
-      <h3>{category}</h3>
-      <p>Risk Probability: {(probability * 100).toFixed(1)}%</p>
-    </div>
-  );
-}
